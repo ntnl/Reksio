859d08d3aa96222ffe39d6063989effd4ff48156
Bartłomiej Syguła <natanael@natanael.krakow.pl>
2011-01-30 23:43:34 +0100

    Initial work: find and load config file

--- lib/Reksio/Core/Config.pm
+++ lib/Reksio/Core/Config.pm
@@ -15,6 +15,8 @@ use strict; use warnings; # {{{
 
 my $VERSION = '0.1.0';
 
+use Carp::Assert::More qw( assert_defined );
+use YAML::Any qw( Load );
 # }}}
 
 my %configuration = ();
@@ -25,6 +27,41 @@ sub set_config_option { # {{{
     $configuration{$option} = $value;
 
     return $value;
+} # }}}
+
+sub find_config { # {{{
+    my @choices = (
+        $ENV{'REKSIO_CONFIG'},
+        q{./.reksio.conf},
+        $ENV{'USER'} . q{/.reksio.conf}, # FIXME: will fail sometimes (probably) due to ENV{USER} being undefined...
+        q{/etc/reksio.conf},
+    );
+
+    foreach my $path (@choices) {
+        if (-f $path) {
+            return $path;
+        }
+    }
+
+    return;
+} # }}}
+
+sub configure { # {{{
+    if ($configuration{'configured'}) {
+        return;
+    }
+
+    my $config_file_path = find_config();
+
+    my $config_src = Load($config_file_path);
+
+    assert_defined($config_src->{'workspace'}, 'workspace is defined');
+
+    # Note:
+    #   Options 'build_results' and 'db' are mandatory for server.
+    #   Workers require only 
+
+    return $configuration{'configured'} = 1;
 } # }}}
 
 # vim: fdm=marker
